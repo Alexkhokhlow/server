037eafaf6978619c49a0ec4f2ab1a5f7a91e14e2
Alexkhokhlow <Lomtik25@gmail.com>
2023-02-07 13:44:18 +0300

    feat: add login request

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcrypt");
 const db = require("../models");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const EXPIRES_IN = "5d";
 
 const User = db.users;
 
@@ -28,6 +30,33 @@ const signup = async (request, response) => {
   }
 };
 
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      where: {
+        email: email,
+      },
+    });
+    if (user) {
+      const isSame = await bcrypt.compare(password, user.password);
+
+      if (isSame) {
+        let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+          expiresIn: EXPIRES_IN,
+        });
+        return res.status(201).send(token);
+      } else {
+        return res.status(401).send("Authentication failed");
+      }
+    }
+    return res.status(401).send("Authentication failed");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 module.exports = {
   signup,
+  login,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
-const { signup } = require("../controllers/controller");
+const { signup, login } = require("../controllers/controller");
 const userAuth = require("../middleware/userAuth");
-
 
 const router = express.Router();
 
 router.post("/signup", userAuth.saveUser, signup);
+
+router.post("/login", login);
 
 module.exports = router;
