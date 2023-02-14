8ad2f431e4bd9eaf1734564122eb3149a5ff7bfc
Alexkhokhlow <Lomtik25@gmail.com>
2023-02-14 13:53:34 +0300

    feat:add dashboard creator

--- models/index.js
+++ models/index.js
@@ -2,16 +2,16 @@ const { Sequelize, DataTypes } = require("sequelize");
 
 
 // for server
-const sequelize = new Sequelize(
-  `postgres://db_0v4n_user:FOLSvtQu3GtoQfcQDbSk9nLz4Phyi1Yc@dpg-cfh05582i3mp5ru4fd8g-a/db_0v4n`,
-  { dialect: "postgres" }
-);
-
-//for local
 // const sequelize = new Sequelize(
-//   `postgres://postgres:Edelweis1@localhost:5432/postgres`,
+//   `postgres://db_0v4n_user:FOLSvtQu3GtoQfcQDbSk9nLz4Phyi1Yc@dpg-cfh05582i3mp5ru4fd8g-a/db_0v4n`,
 //   { dialect: "postgres" }
 // );
+
+// for local
+const sequelize = new Sequelize(
+  `postgres://postgres:Edelweis1@localhost:5432/postgres`,
+  { dialect: "postgres" }
+);
 
 sequelize
   .authenticate()
@@ -26,7 +26,10 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-tables = require("./userModel")(sequelize, DataTypes);
-db.users = tables.User;
+db.users = require("./userModel")(sequelize, DataTypes).User;
+db.dashboard = require("./dashboardModel")(sequelize, DataTypes).DashBoard;
+db.users.hasMany(db.dashboard);
+db.users.belongsToMany(db.dashboard, {through: "user_dashboard"});
+db.dashboard.belongsToMany(db.users, {through: "user_dashboard"});
 
 module.exports = db;
--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,25 @@
 const express = require("express");
 
-const { signup, login } = require("../controllers/controller");
+const {
+  controllerUser,
+  controllerDashboard,
+} = require("../controllers/controllers");
 const userAuth = require("../middleware/userAuth");
 
 const router = express.Router();
 
-router.post("/signup", userAuth.saveUser, signup);
+router.post("/signup", userAuth.saveUser, controllerUser.signup);
 
-router.post("/login", login);
+router.post("/login", controllerUser.login);
+
+router.post("/email", controllerUser.checkEmail);
+
+router.post("/dashboard", controllerDashboard.createDashboard);
+
+router.post("/dashboard/:pathName", controllerDashboard.getDashboard);
+
+router.post("/dashboards", controllerDashboard.getDashboards);
+
+router.post("/users_dashboard", controllerDashboard.addUserToDashboard);
 
 module.exports = router;
