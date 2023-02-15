5aba7e392ceb5c0bafb10524d19bdb2bb125ae2d
Alexkhokhlow <Lomtik25@gmail.com>
2023-02-15 14:03:20 +0300

    fix: server data

--- models/index.js
+++ models/index.js
@@ -1,16 +1,16 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
 // for server
-// const sequelize = new Sequelize(
-//   `postgres://db_0v4n_user:FOLSvtQu3GtoQfcQDbSk9nLz4Phyi1Yc@dpg-cfh05582i3mp5ru4fd8g-a/db_0v4n`,
-//   { dialect: "postgres" }
-// );
-
-// for local
 const sequelize = new Sequelize(
-  `postgres://postgres:Edelweis1@localhost:5432/postgres`,
+  `postgres://db_0v4n_user:FOLSvtQu3GtoQfcQDbSk9nLz4Phyi1Yc@dpg-cfh05582i3mp5ru4fd8g-a/db_0v4n`,
   { dialect: "postgres" }
 );
+
+// for local
+// const sequelize = new Sequelize(
+//   `postgres://postgres:Edelweis1@localhost:5432/postgres`,
+//   { dialect: "postgres" }
+// );
 
 sequelize
   .authenticate()
